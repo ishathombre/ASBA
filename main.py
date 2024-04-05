d42662eb30976ea03fafdddc063fd9a6a8d10d10
Nikav7 <112890224+Nikav7@users.noreply.github.com>
2024-04-05 22:33:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from datasets import Dataset
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from transformers import RobertaTokenizer, RobertaModel, RobertaForSequenceClassification
 from sklearn.model_selection import train_test_split
 import evaluate
 from transformers import TrainingArguments, Trainer
